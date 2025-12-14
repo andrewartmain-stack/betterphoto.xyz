03281e564841d20f15e10d09d4255785db13d1d0
andrewartmain-stack <andrewartmain@gmail.com>
2025-12-14 19:28:16 +0000

    update

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -5,29 +5,24 @@ export default function SuccessPage() {
     const [gid, setGid] = useState<string | null>(null);
 
     useEffect(() => {
-        // ✅ только на клиенте
         const storedGid = localStorage.getItem('generationId');
         if (storedGid) {
             setGid(storedGid);
-            // сразу начинаем скачивание
-            window.location.href = `/api/download?gid=${storedGid}`;
         }
     }, []);
+
+    const downloadImage = async () => {
+        const res = await fetch(`/api/download?gid=${gid}`);
+        return res;
+    }
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
             <p className="text-gray-600 text-lg mb-2">Thank you for your payment!</p>
-            <p className="text-gray-400 text-sm">
-                Your download will start automatically…
-            </p>
             {gid && (
-                <p className="text-gray-400 text-sm mt-2">
-                    If the download doesn’t start, click{' '}
-                    <a className="underline" href={`/api/download?gid=${gid}`}>
-                        here
-                    </a>
-                    .
-                </p>
+                <button onClick={downloadImage} className="inline-flex items-center gap-2 px-6 py-3 bg-black text-white rounded-sm disabled:opacity-50" >
+                    Download image without watermark
+                </button>
             )}
         </div>
     );
